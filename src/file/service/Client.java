5a13c04eff78682197cd7cf97ec45833b27858a2
DrewH <adheiss@bsu.edu>
2022-10-24 11:45:25 -0400

    fix commit

--- src/file/service/Client.java
+++ src/file/service/Client.java
@@ -12,57 +12,56 @@ public class Client {
         Client client = new Client();
         String filePath;
         String fileName;
-        String newFileName;
-        try {
-            loop: while(true) {
+
+
+        loop:
+        while (true) {
+            try {
                 Socket socket = new Socket(InetAddress.getByName("localhost"), 6000);
                 String command = client.getCommand();
-                String response;
                 client.sendCommandToServer(socket, command);
-                switch(command) {
+                switch (command) {
                     case "upload":
                         fileName = client.getFileName();
                         filePath = "ClientFiles/UploadedFiles/" + fileName;
                         client.sendCommandToServer(socket, fileName);
                         client.upload(socket, filePath);
-                        response = client.getMessageFromServer(socket);
-                        System.out.println(response);
+                        client.getMessageFromServer(socket);
                     case "download":
                         fileName = client.getFileName();
                         filePath = "ServerFiles/UploadedFiles/" + fileName;
                         client.sendCommandToServer(socket, filePath);
                         client.download(socket, fileName);
-                        response = client.getMessageFromServer(socket);
-                        System.out.println(response);
+                        break;
+//                        client.getMessageFromServer(socket);
                     case "delete":
-                        fileName= client.getFileName();
+                        fileName = client.getFileName();
                         client.sendCommandToServer(socket, fileName);
-                        response = client.getMessageFromServer(socket);
-                        System.out.println(response);
+                        client.getMessageFromServer(socket);
                     case "rename":
                         break;
                     case "list":
-                        client.list(socket);
-                        response = client.getMessageFromServer(socket);
-                        System.out.println(response);
+                        break;
                     case "quit":
                         socket.close();
-                    case "":
+                        break loop;
+                    default:
+                        client.getMessageFromServer(socket);
+                        break;
                 }
+            } catch(IOException e) {
+                System.out.println(e.getMessage());
             }
 
-
-        } catch(IOException e) {
-            System.out.println(e.getMessage());
         }
 
 
     }
 
-    private String getMessageFromServer(Socket socket) throws IOException {
+    private void getMessageFromServer(Socket socket) throws IOException {
         InputStream input = socket.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        return reader.readLine();
+        System.out.println(reader.readLine());
     }
 
     private String getCommand() {
@@ -77,25 +76,23 @@ public class Client {
         return scanner.nextLine();
     }
 
-    private String getNewFileName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter file name:");
-        return scanner.nextLine();
-    }
 
-
-
-    private void upload(Socket socket, String filePath) throws IOException {
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(filePath));
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
-        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
-        int length;
-        while((length = bufferedInputStream.read(bytes)) != -1) {
-            bufferedOutputStream.write(bytes, 0, length);
-            bufferedOutputStream.flush();
+    private void upload(Socket socket, String filePath) {
+        try {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(filePath));
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            int length;
+            while ((length = bufferedInputStream.read(bytes)) != -1) {
+                bufferedOutputStream.write(bytes, 0, length);
+                bufferedOutputStream.flush();
+            }
+            bufferedOutputStream.close();
+            bufferedInputStream.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-        bufferedOutputStream.close();
-        bufferedInputStream.close();
+
     }
 
     private void sendCommandToServer(Socket socket, String command) throws IOException {
@@ -104,36 +101,23 @@ public class Client {
         writer.println(command);
     }
 
-    private void download(Socket socket, String fileName) throws IOException {
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("ClientFiles/DownloadedFiles/" + fileName));
+    private void download(Socket socket, String fileName) {
+        try {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("ClientFiles/DownloadedFiles/" + fileName));
 
-        byte[] bytes = new byte[8000];
-        int length;
-        while ((length = bufferedInputStream.read(bytes)) != -1) {
-            bufferedOutputStream.write(bytes, 0, length);
+            byte[] bytes = new byte[8000];
+            int length;
+            while ((length = bufferedInputStream.read(bytes)) != -1 ) {
+                bufferedOutputStream.write(bytes, 0, length);
+            }
+
+            bufferedOutputStream.close();
+            bufferedInputStream.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
 
-        bufferedOutputStream.close();
-        bufferedInputStream.close();
     }
-    private void list(Socket socket) throws IOException {
-        InputStream input = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-
-        }
-        input.close();
-        reader.close();
-        System.out.println("Listed all current files");
-
-
-
-    }
-
 
 }
