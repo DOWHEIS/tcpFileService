3be7b667a97d947e4285cbdabaff9410aa913398
DrewH <adheiss@bsu.edu>
2022-12-05 12:40:25 -0500

    add threaded

--- src/file/service/Server.java
+++ src/file/service/Server.java
@@ -21,149 +21,176 @@ public class Server {
         int port = Integer.parseInt(args[0]);
         try {
             Server server = new Server(port);
-            server.service(server);
+            server.serverSocket.setReuseAddress(true);
+
+            while(true) {
+                Socket client = server.serverSocket.accept();
+
+                System.out.println("New Client");
+                Service service = new Service(client);
+
+                new Thread(service).start();
+            }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
     }
 
-    private void service(Server server) {
 
-        loop:
-        while (true) {
-            try {
-                Socket socket = serverSocket.accept();
-                System.out.println("Connected");
 
-                InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+    private static class Service implements Runnable {
+        private final Socket clientSocket;
 
-                String command = reader.readLine();
-                System.out.println("Command: " + command);
-                String fileName;
-                switch (command) {
-                    case "upload":
-                        fileName = reader.readLine();
-                        server.upload(socket, fileName);
-                        break;
-                    case "download":
-                        String filePath = reader.readLine();
-                        server.download(filePath, socket);
-                        break;
-                    case "delete":
-                        fileName = reader.readLine();
-                        server.delete(socket, fileName);
-                        break;
-                    case "rename":
-                        fileName = reader.readLine();
-                        String[] fileNames = fileName.split("&");
-                        server.rename(socket, fileNames[0], fileNames[1]);
-                        break;
-                    case "list":
-                        server.list(socket);
-                        break;
-                    case "quit":
-                        serverSocket.close();
-                        break loop;
-                    default:
-                        server.sendMessageToClient(socket, "Server: Invalid command");
-                        break;
-                }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
+        private Service(Socket clientSocket) {
+            this.clientSocket = clientSocket;
         }
 
 
-    }
+        public void run() {
+            loop:
+            while (true) {
+                try {
+//                    Socket socket = clientSocket.accept();
+//                    System.out.println("Connected");
 
-    private void download(String filePath, Socket server) {
-        try {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(filePath));
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(server.getOutputStream());
-            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+                    InputStream input = clientSocket.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                    String command = reader.readLine();
+                    System.out.println("Command: " + command);
+                    String fileName;
+                    switch (command) {
+                        case "upload":
+                            fileName = reader.readLine();
+                            this.upload(clientSocket, fileName);
+                            break;
+                        case "download":
+                            String filePath = reader.readLine();
+                            this.download(filePath, clientSocket);
+                            break;
+                        case "delete":
+                            fileName = reader.readLine();
+                            this.delete(clientSocket, fileName);
+                            break;
+                        case "rename":
+                            fileName = reader.readLine();
+                            String[] fileNames = fileName.split("&");
+                            this.rename(clientSocket, fileNames[0], fileNames[1]);
+                            break;
+                        case "list":
+                            this.list(clientSocket);
+                            break;
+                        case "quit":
+                            clientSocket.close();
+                            break loop;
+                        default:
+                            this.sendMessageToClient(clientSocket, "Server: Invalid command");
+                            break;
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        public void upload(Socket server, String fileName) throws IOException {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(server.getInputStream());
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("ServerFiles/UploadedFiles/" + fileName));
+
+            byte[] bytes = new byte[8000];
             int length;
             while ((length = bufferedInputStream.read(bytes)) != -1) {
                 bufferedOutputStream.write(bytes, 0, length);
-                bufferedOutputStream.flush();
+            }
+            bufferedOutputStream.close();
+            bufferedInputStream.close();
+            server.close();
+            //this will not send because socket is closed
+            this.sendMessageToClient(server, "Server: " + fileName + " uploaded!");
+        }
+
+        private void delete(Socket server, String fileName) {
+
+            File fileToDelete = new File("ServerFiles/UploadedFiles/" + fileName);
+
+            if (fileToDelete.delete()) {
+                this.sendMessageToClient(server, "Server: " + fileName + " deleted!");
+            } else {
+                this.sendMessageToClient(server, "Server: Failed to delete " + fileName);
             }
 
-            bufferedInputStream.close();
-            bufferedOutputStream.close();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
         }
 
-    }
-
-    public void upload(Socket server, String fileName) throws IOException {
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(server.getInputStream());
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("ServerFiles/UploadedFiles/" + fileName));
-
-        byte[] bytes = new byte[8000];
-        int length;
-        while ((length = bufferedInputStream.read(bytes)) != -1) {
-            bufferedOutputStream.write(bytes, 0, length);
-        }
-        bufferedOutputStream.close();
-        bufferedInputStream.close();
-        this.sendMessageToClient(server, "Server: " + fileName + " uploaded!");
-    }
-
-    private void delete(Socket server, String fileName) {
-
-        File fileToDelete = new File("ServerFiles/UploadedFiles/" + fileName);
-
-        if (fileToDelete.delete()) {
-            this.sendMessageToClient(server, "Server: " + fileName + " deleted!");
-        } else {
-            this.sendMessageToClient(server, "Server: Failed to delete " + fileName);
-        }
-
-    }
-
-    private void sendMessageToClient(Socket server, String message) {
-        try {
+        private void sendMessageToClient(Socket server, String message) {
+            try {
 //            serverSocket.accept();
-            OutputStream output = server.getOutputStream();
+                OutputStream output = server.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                writer.println(message);
+                output.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        private void rename(Socket server, String fileName, String newFileName){
+            File oldFile = new File("ServerFiles/UploadedFiles/" + fileName);
+            File newFile = new File("ServerFiles/UploadedFiles/" + newFileName);
+            if (oldFile.renameTo(newFile)){
+                this.sendMessageToClient(server, "Server: " + fileName + " successfully renamed to " + newFileName);
+            }else {
+                this.sendMessageToClient(server, "Server: Failed to rename " + fileName + " to " + newFileName);
+            }
+
+        }
+        private void list (Socket socket) throws IOException {
+            OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
-            writer.println(message);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+            String path = "ServerFiles/UploadedFiles/";
+            File directory = new File(path);
+            String[] fileList = directory.list();
+            if(fileList!=null)
+                for (String s : fileList) {
+                    System.out.println("-".repeat(24));
+                    writer.println("|" + s + "|");
+                    System.out.println("-".repeat(24));
+
+                }
+            output.close();
+            writer.close();
+            System.out.println("\nListed all current files");
+
+
+
         }
 
-    }
-    private void rename(Socket server, String fileName, String newFileName){
-        File oldFile = new File("ServerFiles/UploadedFiles/" + fileName);
-        File newFile = new File("ServerFiles/UploadedFiles/" + newFileName);
-        if (oldFile.renameTo(newFile)){
-            this.sendMessageToClient(server, "Server: " + fileName + " successfully renamed to " + newFileName);
-        }else {
-            this.sendMessageToClient(server, "Server: Failed to rename " + fileName + " to " + newFileName);
-        }
+        private void download(String filePath, Socket server) {
+            try {
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(filePath));
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(server.getOutputStream());
+                byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+                int length;
+                while ((length = bufferedInputStream.read(bytes)) != -1) {
+                    bufferedOutputStream.write(bytes, 0, length);
+                    bufferedOutputStream.flush();
+                }
 
-    }
-    private void list (Socket socket) throws IOException {
-        OutputStream output = socket.getOutputStream();
-        PrintWriter writer = new PrintWriter(output, true);
-        String path = "ServerFiles/UploadedFiles/";
-        File directory = new File(path);
-        String[] fileList = directory.list();
-        if(fileList!=null)
-        for (String s : fileList) {
-            System.out.println("-".repeat(24));
-            writer.println("|" + s + "|");
-            System.out.println("-".repeat(24));
+                bufferedInputStream.close();
+                bufferedOutputStream.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
 
         }
-        output.close();
-        writer.close();
-        System.out.println("\nListed all current files");
 
 
 
     }
+
+
+
 
 
 }
